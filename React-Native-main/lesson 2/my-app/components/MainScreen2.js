d49ffe8ca023f5c5d2bfdbcb664bb310965ba2e9
AndiRrahmani <andirrahmani28@gmail.com>
2025-11-06 20:46:17 +0100

    lesson 2

---
+++ React-Native-main/lesson 2/my-app/components/MainScreen2.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Text, Stylesheet, View} from "react-native"
+
+
+
+const MainScreen2=()=>{
+
+    return(
+        <View>
+            <Text style={Stylesheet.textStyle}>This is main screen 2</Text>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 30,
+
+    },
+
+
+
+
+})
